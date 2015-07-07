668799554cd0b5372a89cd09c599a6f595e9832c
el-doble <el-doble@users.noreply.github.com>
2015-07-07 17:53:37 +0100

    Update largest_prime_factor.js

--- largest_prime_factor.js
+++ largest_prime_factor.js
@@ -3,7 +3,6 @@ function largestPrimeFactor(number) {
     for (i = 2; i <= num; i++) {
         if (num % i === 0) {
             primeFactor = i;
-            num = num / i;
             console.log(num);
         }
     }
